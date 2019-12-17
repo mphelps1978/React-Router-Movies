5360696df6a523d0a5837908ee053636a59db67a
mphelps1978 <phelps.michael@live.com>
2019-12-17 09:56:06 -0600

    Got the movielist populated. The rest I'll have to learn today..

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+        <Route path="/movie/:id">
+          <Movie />
+        </Route>
+      </div>
     </div>
   );
 };
